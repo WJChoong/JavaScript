154d1f607c230071c56f9b804b13364d75df740c
WJChoong <61902435+WJChoong@users.noreply.github.com>
2020-11-04 16:59:39 +0800

    initial

---
+++ Learn/Loop/loop.js
@@ -0,0 +1,15 @@
+//While loop
+/*
+var num = 0;
+while (num < 100) {
+    num += 1;
+    console.log(num);
+}
+*/
+
+//Forr loop
+/*
+for (let num = 0; num < 50; num++) {
+    console.log(num);
+}
+*/
